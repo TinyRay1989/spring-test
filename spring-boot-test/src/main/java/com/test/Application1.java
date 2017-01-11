a0fdd5c340d1715864605c6aa51dbfebc431161a
yanlei <yanlei0518@163.com>
2017-01-12 02:02:13 +0800

    增加注释

--- spring-boot-test/src/main/java/com/test/Application1.java
+++ spring-boot-test/src/main/java/com/test/Application1.java
@@ -3,6 +3,16 @@ package com.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**  
+ * @Description: spring boot v1.2  里面出现的注解 
+ *     等同
+ * <br>@Configuration
+ * <br>@ComponentScan
+ * <br>@EnableAutoConfiguration
+ * @author yanlei
+ * @date 2017年1月12日 上午1:59:54
+ * @version V1.0
+ */ 
 @SpringBootApplication
 public class Application1 {
 	public static void main(String[] args) {
